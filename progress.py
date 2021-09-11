85507f8ee8a34764dbf40a8c199e2ad7b88b4184
Erik Sandgren <erik.sandgren92@gmail.com>
2021-09-11 08:36:52 +0200

    updates to progress.py

--- progress.py
+++ progress.py
@@ -1,31 +1,30 @@
 #!python
 import re
 import datetime
+
+def duration_from_line(line):
+    h = int(line[1:3])
+    m = int(line[4:6])
+    s = int(line[7:9])
+    return h * (60**2) + m * 60 + s
+
 with open("Readme.md", "r") as f:
     readme = f.read()
 durations = re.findall("\[\d{2}:\d{2}:\d{2}\]", readme)
-seconds_tot = 0
-for dur in durations:
-    h = int(dur[1:3])
-    m = int(dur[4:6])
-    s = int(dur[7:9])
-    seconds_tot += h * (60**2) + m * 60 + s
+seconds_tot = sum((duration_from_line(l) for l in durations))
 
-print("Total time duration:", datetime.timedelta(seconds=seconds_tot, ))
 print("~~~~ Watched videos ~~~~")
 completed_vids = [x.replace("~~", "") for x in re.findall("~~.*~~", readme)]
 for vid in completed_vids:
-    print(vid)
+    print(vid, "\n")
 
-seconds_done = 0
-for dur in durations[len(completed_vids):]:
-    h = int(dur[1:3])
-    m = int(dur[4:6])
-    s = int(dur[7:9])
-    seconds_done += h * (60**2) + m * 60 + s
-print("Watched time duration:", datetime.timedelta(seconds=seconds_done))
+seconds_done = sum((duration_from_line(l) for l in durations[:len(completed_vids)]))
 
-print(f"Completion rate: {round( 100 * seconds_done / seconds_tot)}%")
+print(f"Total videos:   {len(durations)}, total duration:   {datetime.timedelta(seconds=seconds_tot)}")
+print(f"Watched videos: {len(completed_vids)}, watched duration: {datetime.timedelta(seconds=seconds_done)}")
+
+print(f"Completion rate (viewing time): {round( 100 * seconds_done / seconds_tot)}%")
+print(f"Completion rate (video count):  {round( 100  * len(completed_vids) / len(durations))}%")
 
 
 
